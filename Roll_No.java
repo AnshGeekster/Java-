661c9f583e267a3559cdc45432d19b07e734bd82
Ansh Sengar <145358921+AnshGeekster@users.noreply.github.com>
2024-07-26 21:26:25 +0530

    Update Roll_No.java

--- Roll_No.java
+++ Roll_No.java
@@ -5,27 +5,21 @@ public class Roll_No {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // Read the number of elements
         System.out.println("Enter the no. of roll nos. to be taken:");
         int n = sc.nextInt();
         
-        // Create an array to store the roll numbers
         int[] rollNumbers = new int[n];
         
-        // Read the sorted list of roll numbers
         System.out.println("Enter the sorted list of roll nos.:");
         for (int i = 0; i < n; i++) {
             rollNumbers[i] = sc.nextInt();
         }
         
-        // Read the number to search for
         System.out.println("Enter the roll no. to search for:");
         int N = sc.nextInt();
         sc.close();
-        // Perform linear search and get the number of comparisons
         int comparisons = linearSearch(rollNumbers, N);
         
-        // Print the result
         if (comparisons == -1) {
             System.out.println("-1");
         } else {
@@ -33,7 +27,6 @@ public class Roll_No {
         }
     }
     
-    // Linear search method
     public static int linearSearch(int[] array, int target) {
         int comparisons = 0;
         
@@ -44,6 +37,6 @@ public class Roll_No {
             }
         }
         
-        return -1; // Element not found
+        return -1; 
     }
 }
