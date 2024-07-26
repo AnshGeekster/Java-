3176e462af5ec8821c2bb4b86f75e6f09b3069df
Ansh Sengar <145358921+AnshGeekster@users.noreply.github.com>
2024-07-26 21:18:58 +0530

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -8,6 +8,7 @@ public class Distance
         float g= 9.81f;
         System.out.println("Enter the time in seconds: ");
         int X=sc.nextInt();
+        sc.close();
         float d= (0.5f)*g*X*X;
         System.out.println(d+" meters");
     
