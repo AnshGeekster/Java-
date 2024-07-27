b36b612831a8365414788408e89d8efeae2ca192
Ansh Sengar <145358921+AnshGeekster@users.noreply.github.com>
2024-07-27 12:54:52 +0530

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -4,11 +4,20 @@ public class Distance
 
     public static void main(String[] args) 
     {
+         // Create a Scanner object for input
         Scanner sc = new Scanner(System.in);
+        
+        // Define the acceleration due to gravity in meters per second squared
         float g= 9.81f;
         System.out.println("Enter the time in seconds: ");
+        
+        // Read the user input (time in seconds)
         int X=sc.nextInt();
+
+        // Close the Scanner object
         sc.close();
+
+        // Calculate the distance using the formula d = 0.5 * g * t^2
         float d= (0.5f)*g*X*X;
         System.out.println(d+" meters");
     
