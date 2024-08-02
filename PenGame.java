15e94559ddc5bfdf63ab51f91916e118433ed7e7
Ansh Sengar <145358921+AnshGeekster@users.noreply.github.com>
2024-08-03 00:26:40 +0530

    Update PenGame.java

--- PenGame.java
+++ PenGame.java
@@ -3,12 +3,21 @@ import java.util.Random;
 
 public class PenGame {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Display a welcome message to the player
         System.out.println("Welcome to the Pen Game!");
+
+        // Set the minimum and maximum number of pens
         int min =2;
         int max=30;
+
+        // Generate a random number of total pens within the range [min, max]
         int totalPens= (int)(Math.random()*(max-min+1)+min);
+
+        // Display the total number of pens
         System.out.println("Total pens are "+ totalPens);
         while (totalPens > 0) {
       
@@ -16,9 +25,12 @@ public class PenGame {
             if (computerPick == 0) {
                 computerPick = 1;
             }
+
+            // Subtract the computer's pick from the total pens
             totalPens -= computerPick;
             System.out.println("Computer picked " + computerPick + " pens. " + totalPens + " pens remaining.");
 
+            // Check if there are no pens left after the computer's move
             if (totalPens == 0) {
                 System.out.println("Computer picked the last pen. Computer wins!");
                 break;
@@ -32,9 +44,12 @@ public class PenGame {
                 userPick = sc.nextInt();
             }
 
+            // Subtract the player's pick from the total pens
             totalPens -= userPick;
             System.out.println("You picked " + userPick + " pens. " + totalPens + " pens remaining.");
 
+            
+            // Check if there are no pens left after the player's move
             if (totalPens == 0) {
                 System.out.println("You picked the last pen. You win!");
                 break;
